857af9e673c0f4006888092842aba3b4bb3c4a8c
Alex Licursi <alicursi@gmail.com>
2015-02-01 21:23:19 -0500

    Initial commit

---
+++ csv-split-by-column.py
@@ -0,0 +1,41 @@
+
+import sys
+import csv
+import os
+
+import ipdb #for debugging
+
+
+#inputs
+input_filename = 'samples/input.csv'
+output_filename_template = ''
+split_on_column = 'county'
+
+run_directory = os.path.dirname(os.path.abspath(__file__))
+output_directory = run_directory
+
+
+# open input csv
+with open(input_filename, 'rU') as input_file:
+
+	reader = csv.DictReader(input_file)
+
+	for row in reader:
+		output_filename = os.path.join(output_directory,row[split_on_column] + '.csv')
+
+		if os.path.exists(output_filename) == False:
+
+			output_file = open(output_filename,'wb')
+			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
+			writer.writeheader()
+
+		else:
+
+			output_file = open(output_filename,'a')
+			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
+
+		
+		writer.writerow(row)
+
+
+#try with stdin
