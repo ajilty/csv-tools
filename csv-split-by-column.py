5b2d642075a43cea0de5366540ce841e92200a18
Alex Licursi <alicursi@gmail.com>
2015-02-02 23:06:04 -0500

    more

--- csv-split-by-column.py
+++ csv-split-by-column.py
@@ -15,15 +15,15 @@ with open(input_filename, 'rU') as input_file:
 
 	reader = csv.DictReader(input_file)
 
-	for row in reader:
-		output_filename = os.path.join(output_directory,row[split_on_column] + '.csv')
+	for input_row in reader:
+		output_filename = os.path.join(output_directory,input_row[split_on_column] + '.csv')
 
 		if os.path.exists(output_filename) == False:
-			output_file = open(output_filename,'wb')
+			output_file = open(output_filename,'w')
 			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
 			writer.writeheader()
 		else:
 			output_file = open(output_filename,'a')
 			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
 		
-		writer.writerow(row)
+		writer.writerow(input_row)
