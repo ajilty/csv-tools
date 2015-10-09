c32240499bb6252696e755ff54ff86db8125bc2a
Alex Licursi <alicursi@gmail.com>
2015-10-09 07:49:01 -0400

    Styling improvements

--- csv-split-by-column.py
+++ csv-split-by-column.py
@@ -1,7 +1,5 @@
-import sys
 import csv
 import os
-import ipdb #for debugging
 
 input_filename = 'samples/input.csv'
 output_filename_template = ''
@@ -16,14 +14,25 @@ with open(input_filename, 'rU') as input_file:
 	reader = csv.DictReader(input_file)
 
 	for input_row in reader:
-		output_filename = os.path.join(output_directory,input_row[split_on_column] + '.csv')
+		output_filename = os.path.join(
+			output_directory,
+			input_row[split_on_column] + '.csv'
+		)
 
-		if os.path.exists(output_filename) == False:
-			output_file = open(output_filename,'w')
-			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
+		if os.path.exists(output_filename) is False:
+			output_file = open(output_filename, 'w')
+			writer = csv.DictWriter(
+				output_file,
+				delimiter=',',
+				fieldnames=reader.fieldnames
+			)
 			writer.writeheader()
 		else:
-			output_file = open(output_filename,'a')
-			writer = csv.DictWriter(output_file, delimiter=',', fieldnames=reader.fieldnames)
-		
+			output_file = open(output_filename, 'a')
+			writer = csv.DictWriter(
+				output_file,
+				delimiter=',',
+				fieldnames=reader.fieldnames
+			)
+
 		writer.writerow(input_row)
